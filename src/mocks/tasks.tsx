bc2a82161d31a7ab584f4ee9ae26ada98dd97b75
Diego Sano <diego.sano@nave.rs>
2022-07-14 01:06:58 -0300

    feat: mocked tasks

---
+++ src/mocks/tasks.tsx
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const TASKS = [
+  {
+    id: uuidv4(),
+    content: 'Read a book',
+    concluded: false,
+  },
+  {
+    id: uuidv4(),
+    content: 'Study React',
+    concluded: false,
+  },
+  {
+    id: uuidv4(),
+    content: 'Prepare for interviews',
+    concluded: true,
+  },
+]
