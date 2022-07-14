1a2cb4d258df2b19afa11434321bd3d8759ff3be
Diego Sano <diego.sano@nave.rs>
2022-07-14 01:07:09 -0300

    feat: add new components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
 import { useState } from 'react'
-import { Header } from './components/Header'
+import { v4 as uuidv4 } from 'uuid'
 
+import { Header } from './components/Header'
+import { NewTask } from './components/NewTask'
+import { Tasks } from './components/Tasks'
+
+import styles from './App.module.css'
 import './global.css'
 
+import { TASKS } from './mocks/tasks'
+
+interface Task {
+  id: string;
+  content: string;
+  concluded: boolean;
+}
+
 export function App() {
+  const [tasks, setTasks] = useState<Task[]>(TASKS)
+
+  function handleOnAddTask(task: string) {
+    const newTask = {
+      id: uuidv4(),
+      content: task,
+      concluded: false,
+    }
+
+    setTasks(state => [...state, newTask])
+  }
+
+  function handleOnConcludeTask(id: string, concluded: boolean) {
+    setTasks(state => state.map(task => {
+      if (task.id === id) {
+        task.concluded = concluded
+      }
+
+      return task
+    }))
+  }
+
+  function handleOnDeleteTask(id: string) {
+    setTasks(state => state.filter(task => task.id !== id))
+  }
+
   return (
-    <Header />
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <main>
+          <NewTask onAddTask={handleOnAddTask} />
+
+          <Tasks
+            tasks={tasks}
+            onConcludeTask={handleOnConcludeTask}
+            onDeleteTask={handleOnDeleteTask}
+          />
+        </main>
+      </div>
+    </>
   )
 }
